0fbb3322c8beb3391f4686c89ea3fda3a240844d
Aors <aorsundet@outlook.com>
2023-06-14 20:44:43 +0200

    deleted unused files

--- src/vat_return_client/__init__.py
+++
@@ -1,9 +0,0 @@
-from .client import VatReturn
-from ._utils import get_maskinporten_token
-# from ._config import config
-
-__all__ = [
-    "VatReturn",
-    "get_maskinporten_token",
-    # "config"
-]
